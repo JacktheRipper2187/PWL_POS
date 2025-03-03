d15565a1aa9a7ee0f3a8ad4d520cb60def71b938
Ulil Fahmi <ulilf442@gmail.com>
2025-03-03 13:21:40 +0700

    Jobsheet04_Praktikum 1 - fillable:

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
@@ -18,14 +17,22 @@ class UserController extends Controller
         // UserModel::insert($data); //tambahkan data ke tabel m_user
 
         //tambah data uset dengan Eloquent Model
-        $data =[
-            'nama' => 'Pelanggan Pertama',
+        // $data =[
+        //     'nama' => 'Pelanggan Pertama',
 
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data); //update data user
+
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data); //update data user
+        UserModel::create($data);
 
         //coba akses model UserModel
-        $user = userModel::all(); //ambil semua data dari tabel m_user
+        $user = UserModel::all(); //ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }
 }
