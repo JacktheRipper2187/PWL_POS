d668e657ebc836d25c0a46505f8ee380f8cd068f
Ulil Fahmi <ulilf442@gmail.com>
2025-03-06 12:47:33 +0700

    Praktikum 2.1 – Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,38 +1,38 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
+
 class UserController extends Controller
 {
-    public function index(){
-        // //tambah data user dengan Eloquent Model
+    public function index()
+    {
+        // Add user data with Eloquent Model
         // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
         // ];
-        // UserModel::insert($data); //tambahkan data ke tabel m_user
+        // UserModel::create($data);
 
-        //tambah data uset dengan Eloquent Model
-        // $data =[
-        //     'nama' => 'Pelanggan Pertama',
+        // Access UserModel
+        // $user = UserModel::find(1); // Get user with ID 1 from the m_user table
+        // return view('user', ['data' => $user]);
 
-        // ];
-        // UserModel::where('username', 'customer-1')->update($data); //update data user
+        // $user = UserModel::where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);
 
-        //coba akses model UserModel
-        $user = UserModel::all(); //ambil semua data dari tabel m_user
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        });
+
         return view('user', ['data' => $user]);
     }
 }
