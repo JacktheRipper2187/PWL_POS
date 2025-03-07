66c6c54822ca7257fb47dd6dcf7c1c2cd58f132b
Ulil Fahmi <ulilf442@gmail.com>
2025-03-07 10:26:41 +0700

    Praktikum 2.5 – Attribute Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\dd;
 
 class UserController extends Controller
 {
@@ -73,20 +74,62 @@ class UserController extends Controller
         // );
         // return view('user', ['data' => $user]);
 
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'level_id' => 2
-            ]
-        );
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'level_id' => 2
+        //     ]
+        // );
         
-        // Set password only if the user is new
-        if (!$user->exists) {
-            $user->password = Hash::make('12345');
-        }
+        // // Set password only if the user is new
+        // if (!$user->exists) {
+        //     $user->password = Hash::make('12345');
+        // }
         
+        // $user->save();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::create([
+        //     'username' => 'manager55',
+        //     'nama' => 'Manager55',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ]);
+
+        // $user->username = 'manager56';
+
+        // $user->isDirty(); // true
+        // $user->isDirty('username'); // true
+        // $user->isDirty('nama'); // false
+        // $user->isDirty(['nama', 'username']); // true
+
+        // $user->isClean(); // false
+        // $user->isClean('username'); // false
+        // $user->isClean('nama'); // true
+        // $user->isClean(['nama', 'username']); // false
+
+        // $user->save();
+
+        // $user->isDirty(); // false
+        // $user->isClean(); // true
+        // dd($user->isDirty());
+
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+
+        $user->username = 'manager12';
+
         $user->save();
-        return view('user', ['data' => $user]);
+
+        $user->wasChanged();// true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // false
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged(['nama', 'username']));// true
     }
 }
