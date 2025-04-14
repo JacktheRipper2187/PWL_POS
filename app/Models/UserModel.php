d2df8499bf5043dc46018a33f051527f32b16d3b
Ulil Fahmi <ulilf442@gmail.com>
2025-04-14 20:46:50 +0700

    Praktikum2_JS7_Minggu7_Implementasi_Authorization_di_Laravel_dengan_Middleware

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -1,9 +1,8 @@
 <?php
-
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
 
@@ -11,18 +10,30 @@ class UserModel extends Authenticatable
 {
     use HasFactory;
 
-    protected $table = 'm_user'; //mendefinisikan nama tabel yang digunakan oleh model ini 
-    protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel yang digunakan
-    /**
-     * The attributes that are mass assignable
-     * @var array
-     */
-    protected $fillable =['level_id', 'username', 'nama', 'password'];
-    protected $hidden = ['password']; // jangan ditampilkan saat select 
-    protected $casts = ['password' => 'hashed' ]; // casting password agar otomatis di hash 
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
 
+    protected $hidden = ['password']; // jangan di tampilkan saat select
+
+    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
+
+    /**
+     * Relasi ke tabel level
+     */
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
+
+    public function getRoleName(): string
+    {
+        return $this->level->level_name ;
+    }
+
+    public function hasRole($role): bool
+    {
+        return $this->level->level_kode === $role;
+    }
+
 }
